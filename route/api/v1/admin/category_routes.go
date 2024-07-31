08721244472ea97dec6242b3f8e5e1005c3ce934
水告木南 <Haonan_Fang@outlook.com>
2024-08-01 00:34:13 +0800

    feat<route>:更新路由文件

--- route/api/v1/admin/category_routes.go
+++ route/api/v1/admin/category_routes.go
@@ -1,10 +1,20 @@
 package admin
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"sky-take-out-gin/internal/controller/admin/category"
+)
 
 func CategoryRoutes(route *gin.RouterGroup) {
+	//TODO:controller应该不在这里声明,之后需要修改
+	controller := category.NewAdminCategoryControllerImpl()
 	category_route := route.Group("/category")
 	{
-		category_route.PUT("/")
+		category_route.PUT("/", controller.UpdateCategory)
+		category_route.GET("/page", controller.GetCategoryPage)
+		category_route.POST("/status/:status", controller.ChangeCategoryStatus)
+		category_route.POST("/", controller.CreateCategory)
+		category_route.DELETE("/", controller.DeleteCategory)
+		category_route.GET("/list", controller.GetCategoryListByType)
 	}
 }
--- route/route.go
+++ route/route.go
@@ -1,6 +1,9 @@
 package route
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"sky-take-out-gin/route/api/v1/admin"
+)
 
 // SetupHTTPRoute 用于初始化 HTTP 路由
 func SetupHTTPRoute() *gin.Engine {
@@ -9,11 +12,12 @@ func SetupHTTPRoute() *gin.Engine {
 	{
 		UserApi := ApiV1.Group("/user")
 		{
-
+			admin.CategoryRoutes(UserApi)
 		}
 		AdminApi := ApiV1.Group("/admin")
 		{
 
 		}
 	}
+	return route
 }
