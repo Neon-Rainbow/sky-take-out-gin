b2ca46ee3c6794d7f6fcb27e8111007597589a46
水告木南 <Haonan_Fang@outlook.com>
2024-08-01 00:20:20 +0800

    feat:自定义了错误ApiError
    ApiError实现了Error()接口

---
+++ model/api_error.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"fmt"
+	"sky-take-out-gin/code"
+)
+
+type ApiError struct {
+	Code code.ResponseCode `json:"code"`
+	Msg  string            `json:"msg"`
+}
+
+func (e *ApiError) Error() string {
+	return fmt.Sprintf("ApiError: code=%v, msg=%v", e.Code, e.Msg)
+}
