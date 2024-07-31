5504d68cc2082fe38d559201b84156ac20b7a870
水告木南 <Haonan_Fang@outlook.com>
2024-08-01 00:17:12 +0800

    feat:定义错误码
    错误码定义如下:
    服务级错误码
    1 位数进行表示
    其中 1 为系统级错误, 2 为普通错误, 通常是由用户非法操作引起
    
    模块级错误码
    2 位数进行表示
    01: 用户模块错误
    02: 商品模块错误
    
    具体错误码
    2 位数进行表示
    具体根据模块中的操作定义

---
+++ code/code.go
@@ -0,0 +1,75 @@
+// Package code 响应码定义
+package code
+
+import "strconv"
+
+// ResponseCode 响应码
+type ResponseCode int
+
+// GenerateResponseCode 生成响应码
+// @Param ServiceError int 服务级错误码：1 位数进行表示，比如 1 为系统级错误；2 为普通错误，通常是由用户非法操作引起。
+// @Param ModelError int 模块级错误码：2 位数进行表示，比如 01 为用户模块错误；02 为商品模块错误。
+// @Param DetailError int 具体错误码：2 位数进行表示，比如 01 为用户不存在；02 为密码错误。
+// @Return ResponseCode 响应码
+func GenerateResponseCode(ServiceError int, ModelError int, DetailError int) ResponseCode {
+	return ResponseCode(ServiceError*10000 + ModelError*100 + DetailError)
+}
+
+//服务级错误码
+//1 位数进行表示
+//其中 1 为系统级错误, 2 为普通错误, 通常是由用户非法操作引起
+//
+//模块级错误码
+//2 位数进行表示
+//01: 用户模块错误
+//02: 商品模块错误
+//
+//具体错误码
+//2 位数进行表示
+//具体根据模块中的操作定义
+
+const (
+	ServerError        = 10101
+	TooManyRequests    = 10102
+	ParamBindError     = 10103
+	AuthorizationError = 10104
+	UrlSignError       = 10105
+	MySQLExecError     = 10106
+)
+
+var ResponseCodeMessageMap = map[ResponseCode]string{
+	ServerError:        "服务器内部错误",
+	TooManyRequests:    "请求过多",
+	ParamBindError:     "参数信息错误",
+	AuthorizationError: "签名信息错误",
+	UrlSignError:       "参数签名错误",
+	MySQLExecError:     "数据库执行错误",
+}
+
+func (r ResponseCode) Message() string {
+	return ResponseCodeMessageMap[r]
+}
+
+func (r ResponseCode) String() string {
+	return strconv.Itoa(int(r))
+}
+
+func (r ResponseCode) Int() int {
+	return int(r)
+}
+
+func (r ResponseCode) Error() string {
+	return r.Message()
+}
+
+func (r ResponseCode) GetServerErrorCode() int {
+	return int(r / 10000)
+}
+
+func (r ResponseCode) GetModelErrorCode() int {
+	return int(r % 10000 / 100)
+}
+
+func (r ResponseCode) GetDetailErrorCode() int {
+	return int(r % 100)
+}
