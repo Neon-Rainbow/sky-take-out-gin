ecb3d6e5d7f69cd3bd45c318d026f5bc2050da94
水告木南 <Haonan_Fang@outlook.com>
2024-08-02 23:31:22 +0800

    feat: 优化Setmeal模型的定义
    这个提交优化了Setmeal模型的定义，添加了SetmealDishes字段，并设置了外键关联。这样可以更好地管理套餐和套餐菜品的关系。

--- model/sql/setmeal.go
+++ model/sql/setmeal.go
@@ -3,17 +3,18 @@ package model
 import "time"
 
 type Setmeal struct {
-	ID          int64     `json:"id" gorm:"primary_key;autoIncrement"`
-	CategoryID  int64     `json:"category_id"`
-	Name        string    `json:"name"`
-	Price       float64   `json:"price"`
-	Status      int       `json:"status"`
-	Description string    `json:"description"`
-	Image       string    `json:"image"`
-	CreateTime  time.Time `json:"create_time"`
-	UpdateTime  time.Time `json:"update_time"`
-	CreateUser  int64     `json:"create_user"`
-	UpdateUser  int64     `json:"update_user"`
+	ID            int64         `json:"id" gorm:"primary_key;autoIncrement"`
+	CategoryID    int64         `json:"category_id"`
+	Name          string        `json:"name"`
+	Price         float64       `json:"price"`
+	Status        int           `json:"status"`
+	Description   string        `json:"description"`
+	Image         string        `json:"image"`
+	CreateTime    time.Time     `json:"create_time"`
+	UpdateTime    time.Time     `json:"update_time"`
+	CreateUser    int64         `json:"create_user"`
+	UpdateUser    int64         `json:"update_user"`
+	SetmealDishes []SetmealDish `json:"setmeal_dishes" gorm:"foreignKey:SetmealID"`
 }
 
 func (Setmeal) TableName() string {
