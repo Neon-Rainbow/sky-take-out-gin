4b69f9eaf48cee65df8301f771fcecea6ff5b853
水告木南 <Haonan_Fang@outlook.com>
2024-08-03 00:16:57 +0800

    feat:增加套餐模块的Request和Response结构体

---
+++ model/param/admin/setmeal.go
@@ -0,0 +1,75 @@
+package admin
+
+import model "sky-take-out-gin/model/sql"
+
+type UpdateSetmealRequest struct {
+	ID            int64               `json:"id" binding:"required"`          // 套餐id
+	CategoryID    int64               `json:"category_id" binding:"required"` // 分类id
+	Name          string              `json:"name" binding:"required"`        // 套餐名称
+	Price         float64             `json:"price" binding:"required"`       // 套餐价格
+	Status        int                 `json:"status"`                         // 套餐起售状态
+	Description   string              `json:"description"`                    // 套餐描述
+	Image         string              `json:"image"`                          // 套餐图片路径
+	SetmealDishes []model.SetmealDish `json:"setmeal_dishes"`                 // 套餐和菜品关联关系
+}
+
+type UpdateSetmealResponse struct {
+}
+
+type GetSetmealsRequest struct {
+	CategoryID int64  `form:"category_id"`                  // 分类id
+	Name       string `form:"name"`                         // 套餐名称
+	Status     int    `form:"status"`                       // 套餐起售状态
+	Page       int    `form:"page" binding:"required"`      // 页码
+	PageSize   int    `form:"page_size" binding:"required"` // 每页记录数
+}
+
+type GetSetmealsResponse struct {
+	Total   int             `json:"total"`
+	Records []model.Setmeal `json:"records"`
+}
+
+type UpdateSetmealStatusRequest struct {
+	Status int   `uri:"status"` // 套餐状态，1表示起售，0表示停售
+	ID     int64 `form:"id"`    // 套餐id
+}
+
+type UpdateSetmealStatusResponse struct {
+}
+
+type DeleteSetmealsRequest struct {
+	IDs []int64 `form:"ids" binding:"required"` // ids
+}
+
+type DeleteSetmealsResponse struct {
+}
+
+type AddSetmealRequest struct {
+	CategoryID    int64           `json:"category_id" binding:"required"`    // 分类id
+	Name          string          `json:"name" binding:"required"`           // 套餐名称
+	Price         float64         `json:"price" binding:"required"`          // 套餐价格
+	Status        int             `json:"status" binding:"required"`         // 套餐状态：1为起售 0为停售
+	Description   string          `json:"description"`                       // 套餐描述
+	Image         string          `json:"image"`                             // 套餐图片
+	SetmealDishes []model.Setmeal `json:"setmeal_dishes" binding:"required"` // 套餐和菜品关联关系
+}
+
+type AddSetmealResponse struct {
+}
+
+type GetSetmealByIDRequest struct {
+	ID int64 `uri:"id" binding:"required"` // 套餐id
+}
+
+type GetSetmealByIDResponse struct {
+	ID            int64           `json:"id"`
+	CategoryID    int64           `json:"category_id"`
+	Name          string          `json:"name"`
+	Price         float64         `json:"price"`
+	Status        int             `json:"status"`
+	Description   string          `json:"description"`
+	Image         string          `json:"image"`
+	UpdateTime    string          `json:"update_time"`
+	CategoryName  string          `json:"category_name"`
+	SetmealDishes []model.Setmeal `json:"setmeal_dishes"`
+}
