40c9427b58a251f966036a951e46dd6e46657958
水告木南 <Haonan_Fang@outlook.com>
2024-08-02 22:51:18 +0800

    feat: 优化JWT中间件的实现
    这个提交优化了JWT中间件的实现，包括修改参数绑定方式和添加错误码。

--- middleware/JWTMiddleware.go
+++ middleware/JWTMiddleware.go
@@ -1,3 +1,49 @@
 package middleware
 
-//TODO:需要实现JWT中间件
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"sky-take-out-gin/code"
+	"sky-take-out-gin/internal/controller"
+	"sky-take-out-gin/utils/jwt"
+	"strings"
+)
+
+// JWTMiddleware JWT中间件
+func JWTMiddleware() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader == "" {
+			controller.ResponseErrorWithMsg(c, http.StatusUnauthorized, code.RequestUnauthorized, "Authorization header 不能为空")
+			c.Abort()
+			return
+		}
+
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" || parts[1] == "" {
+			controller.ResponseErrorWithMsg(c, http.StatusUnauthorized, code.RequestUnauthorized, "Authorization header 的格式必须为 Bearer {token}")
+
+			c.Abort()
+			return
+		}
+
+		tkn := parts[1]
+		myClaims, err := jwt.ParseToken(tkn)
+		if err != nil {
+			controller.ResponseErrorWithMsg(c, http.StatusUnauthorized, code.RequestUnauthorized, "Token 无效")
+			//httpController.ResponseErrorWithMessage(c, code.RequestUnauthorized, "Invalid token")
+			c.Abort()
+			return
+		}
+
+		if myClaims.TokenType != "access_token" {
+			controller.ResponseErrorWithMsg(c, http.StatusUnauthorized, code.RequestUnauthorized, "无效的token格式,应该为access_token")
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", myClaims.UserID)
+		c.Set("username", myClaims.Username)
+		c.Next()
+	}
+}
